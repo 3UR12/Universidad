6cd5d7191f64201c7d2b190d3c00ad296431fd92
Euris J. Rodríguez V. <ej8187527@gmail.com>
2025-08-11 20:30:05 -0500

    Update database.py
    change rout

--- Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/database.py
+++ Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///orden_magica.db", echo = false)
+engine = create_engine("sqlite:///1 registro de misiones de aventureros", echo = false)
 Session = sessionmaker(bind = engine)
 Session = Session()
+
