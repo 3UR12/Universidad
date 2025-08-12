082e73927fa5a487aa01c7e10e0f8a03efa92888
Euris J. Rodríguez V. <ej8187527@gmail.com>
2025-08-11 20:26:43 -0500

    Update main.py

--- Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/main.py
+++ Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/main.py
@@ -1,1 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("sqlite:///orden_magica.db", echo = false)
+Session = sessionmaker(bind = engine)
+Session = Session()
