1c995621173b454a035072a210f4ec81435c0d8d
Euris J. Rodríguez V. <ej8187527@gmail.com>
2025-08-14 20:38:04 -0500

    Delete Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/database.py

--- Codigos/2025/programacion IV/Tareas/1 registro de misiones de aventureros/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("sqlite:///1 registro de misiones de aventureros", echo = false)
-Session = sessionmaker(bind = engine)
-Session = Session()
-
